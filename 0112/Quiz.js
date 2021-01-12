be2f06c485863dfab3d782e5e8f1a0cfdb8ef82b
Kim Young Ae <kya754@gmail.com>
2021-01-12 23:21:03 +0900

    Update Quiz.js

--- 0112/Quiz.js
+++ 0112/Quiz.js
@@ -3,15 +3,31 @@
 // var list = ['1. 하나', '2. 둘', '3. 셋'];
 // <a href="#">1. 하나</a><a href="#">2. 둘</a><a href="#">3. 셋</a>
 
+
+// forEach, map
+// 공통점 : 배열을 이용한다.
+// 차이점 : forEach는 return 값 못 받음 / map은 가능
+
+
 var list = ['1. 하나', '2. 둘', '3. 셋'];
 var result = '';
 
-var changeList = list.map(function(text){
+
+var forEachVersion = list.forEach((text) => {
 
   result += `<a href="#">${text}</a>`;
+  
+  // return result -> undefined
 });
 
-
 console.log(result);
+
+
+var mapVersion = list.map((text) => {
+
+  return `<a href="#">${text}</a>`;
+});
+
+console.log(mapVersion.join(''));
 
 
